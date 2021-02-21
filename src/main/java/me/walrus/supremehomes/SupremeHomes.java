5a0885cd615094d69b27c523c41f46bead7975a1
xinnir <mersennetwizter@gmail.com>
2021-02-20 23:39:20 -0800

    Initial commit

---
+++ src/main/java/me/walrus/supremehomes/SupremeHomes.java
@@ -0,0 +1,17 @@
+package me.walrus.supremehomes;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SupremeHomes extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
