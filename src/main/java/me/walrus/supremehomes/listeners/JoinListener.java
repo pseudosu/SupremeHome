0b2bc0b96c905c9d7731556215e7b08a5e39d05e
pseudosu <mersennetwizter@gmail.com>
2021-02-22 16:23:10 -0800

    Added update checking

--- src/main/java/me/walrus/supremehomes/listeners/JoinListener.java
+++ src/main/java/me/walrus/supremehomes/listeners/JoinListener.java
@@ -1,20 +1,34 @@
 package me.walrus.supremehomes.listeners;
 
 import me.walrus.supremehomes.network.DatabaseManager;
+import me.walrus.supremehomes.network.UpdateChecker;
+import me.walrus.supremehomes.util.Permissions;
+import me.walrus.supremehomes.util.Util;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.SQLException;
 
 public class JoinListener implements Listener {
 
+    private Plugin plugin;
+
+    public JoinListener(Plugin plugin){
+        this.plugin = plugin;
+    }
+
     @EventHandler
     public void onJoin(PlayerJoinEvent event){
-//        try {
-//            DatabaseManager.initializePlayer(event.getPlayer().getUniqueId().toString());
-//        } catch (SQLException e) {
-//            e.printStackTrace();
-//        }
+        if(event.getPlayer().hasPermission(Permissions.ADMIN)){
+            new UpdateChecker((JavaPlugin) plugin, 89358).getVersion(version -> {
+                if (plugin.getDescription().getVersion().equalsIgnoreCase(version)) {
+                } else {
+                    Util.sendMessage(event.getPlayer(),"&aThere is a new update available for SupremeHomes. Download it here: https://www.spigotmc.org/resources/supremehomes.89358/");
+                }
+            });
+        }
     }
 }
--- src/main/java/me/walrus/supremehomes/util/Permissions.java
+++ src/main/java/me/walrus/supremehomes/util/Permissions.java
@@ -5,4 +5,5 @@ public class Permissions {
     public static final String SET_HOME = "supremehomes.sethome";
     public static final String DELETE_HOME = "supremehomes.delhome";
     public static final String BASE = "supremehomes.base";
+    public static final String ADMIN = "supremehomes.admin";
 }
